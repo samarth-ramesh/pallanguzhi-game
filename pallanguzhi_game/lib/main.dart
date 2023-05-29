a80f7e55892e6e88d14c6516846a9d4d61f7bc7e
Samarth <samarthr1@outlook.com>
2023-05-29 17:42:23 +0000

    add in some basic mechanics

--- pallanguzhi_game/lib/main.dart
+++ pallanguzhi_game/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -31,95 +33,99 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const HomeScreen(),
+      // home: ,
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _HomeScreenState extends State<HomeScreen> {
+  List<int> countA = [0, 0, 0, 0, 0, 0, 0];
+  List<int> countB = [0, 0, 0, 0, 0, 0, 0];
+  int valueA = 36;
+  int valueB = 36;
+  bool isGameRunning = false;
+  bool curTurn = false;
 
-  void _incrementCounter() {
+  void onClickButton(int idx, int key) {
+    List<List<int>> normalizedScheme = [countA, countB];
+    int numIters = normalizedScheme[key][idx];
+    if (idx == 3 || ((key > 0) ^ curTurn) || (numIters == 0)) {
+      // bank, other player, empty set
+
+      return;
+    } else {
+      normalizedScheme[key][idx] = 0;
+      int curKey = key;
+      int curIdx = idx + 1;
+      for (var i = 0; i < numIters; i++) {
+        if (curIdx > 6) {
+          curIdx = curIdx - 7;
+          curKey = curKey > 0 ? 0 : 1;
+        }
+        log("curKey: $curKey, curIdx: $curIdx");
+        normalizedScheme[curKey][curIdx]++;
+        curIdx++;
+      }
+    }
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      countA = normalizedScheme[0];
+      countB = normalizedScheme[1];
+      curTurn = !curTurn;
     });
+  }
+
+  void startGameForPlayer(int count, List<int> board) {
+    setState(() {
+      for (var i = 0; i < 7 && count >= 6; i++) {
+        if (i != 3) {
+          count -= 6;
+          board[i] = 6;
+        }
+      }
+    });
+  }
+
+  void startGame() {
+    startGameForPlayer(valueA, countA);
+    startGameForPlayer(valueB, countB);
+  }
+
+  List<Widget> renderRow(List<int> l, int key) {
+    List<Widget> rv = [];
+    for (var i = 0; i < l.length; i++) {
+      rv.add(TextButton(
+          onPressed: () => {onClickButton(i, key)},
+          child: Text(l[i].toString())));
+    }
+    return rv;
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Center(
+        child: Container(
+      alignment: Alignment.center,
+      child: Column(
+        children: [
+          Row(
+            children: renderRow(countA, 0),
+          ),
+          Row(
+            children: renderRow(countB, 1),
+          ),
+          FilledButton(onPressed: startGame, child: const Text("Start Gamet")),
+          Text("Current Turn is : ${curTurn ? "Down" : "Up"}",
+              style: Typography.englishLike2021.headlineSmall)
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+    ));
   }
 }
